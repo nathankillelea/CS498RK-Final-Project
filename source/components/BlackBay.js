91807e5f1947a7b4f8b39e2f384122a3a228750b
dgjenni2 <dgjenni2@illinois.edu>
2017-12-09 17:35:52 -0600

    Changes done to black bae

--- source/components/BlackBay.js
+++ source/components/BlackBay.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, AppRegistry, TouchableOpacity, Alert, TextInput, KeyboardAvoidingView, Image, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Button, AppRegistry, Modal, TouchableOpacity, Alert, TextInput, KeyboardAvoidingView, Image, StatusBar } from 'react-native';
 
 export default class BlackBay extends React.Component {
 	static navigationOptions = {
@@ -9,9 +9,29 @@ export default class BlackBay extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-
+			showModal: false,
+			newBottleText: "Hello Bottle"
 		}
 	}
+
+	createNewBottle = () => {
+		this.setState({showModal: true});
+	};
+
+	selectBottleList() {
+
+	};
+
+
+
+	closeModal = () => {
+		this.setState({showModal: false});
+	};
+
+	saveModalData = (text) => {
+		this.setState({newBottleText: text})
+	};
+
 	render() {
 		const { navigate } = this.props.navigation;
 		return (
@@ -20,7 +40,43 @@ export default class BlackBay extends React.Component {
 					barStyle="light-content"
 				/>
 				<Text>BLACK BAY</Text>
-    		</View>
+				<Image
+					source={require('../assets/bleckbae2.jpg')}
+				/>
+				<Modal visible={this.state.showModal}>
+					<View style={styles.container}>
+						<TextInput style={styles.input}
+								   placeholder={this.state.newBottleText}
+								   autoCapitalize="none"
+								   autoCorrect={true}
+								   multiline={true}
+								   numberOfLines={10}
+								   blurOnSubmit={true}
+								   onChangeText={this.saveModalData}
+						/>
+						<Button
+							onPress={this.closeModal}
+							title="Keep Bottle"
+							color="#17c11a"
+						/>
+						<Button
+							onPress={this.closeModal}
+							title="Throw Away"
+							color="#c4301d"
+						/>
+					</View>
+					<View/>
+				</Modal>
+
+				<Button
+					onPress={this.createNewBottle}
+					title="Make New Bottle"
+				/>
+				<Button
+					onPress={this.createNewBottle}
+					title="Choose From Collection"
+				/>
+			</View>
     	);
 	}
 }
