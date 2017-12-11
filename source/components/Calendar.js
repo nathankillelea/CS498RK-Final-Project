d30b534d0c98649e6e9cec9d102f59b968e945b4
bombolord <nathan.killelea@gmail.com>
2017-12-11 16:06:59 -0600

    blackbay font

--- source/components/Calendar.js
+++ source/components/Calendar.js
@@ -18,9 +18,9 @@ export default class CalendarView extends React.Component {
 	};
 
 	componentWillMount(){
-        let hostname = "LAPTOP-F1020140"; //Doug's computer
+        let hostname = "Nathan-PC"; //Doug's computer
 
-        let taskEndpt = 'http://' + secrets.access + '/api/tasks?where={\"owner\":\"boog\"}';
+        let taskEndpt = 'http://' + hostname + '/api/tasks?where={\"owner\":\"boog\"}';
         axios.get(taskEndpt)
             .then((response) => {
 
