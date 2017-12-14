fdd628984ad879c4a4eaadd00d0ca4134193921a
aleclindy <alec.lindemann@gmail.com>
2017-12-13 23:19:13 -0600

    Add files via upload

--- source/components/Calendar.js
+++ source/components/Calendar.js
@@ -1,255 +1,161 @@
 import React from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    FlatList,
-    AppRegistry,
-    Button,
-    TouchableOpacity,
-    Alert,
-    TextInput,
-    KeyboardAvoidingView,
-    Image,
-    Modal
-} from 'react-native';
-import {
-    Calendar,
-    CalendarList,
-    Agenda
-} from 'react-native-calendars';
-import {
-    List,
-    ListItem,
-    SearchBar
-} from 'react-native-elements';
-import axios from 'axios'
-
+import {  StyleSheet, Text, View, FlatList, AppRegistry, Button, TouchableOpacity, Alert, TextInput, KeyboardAvoidingView, Image, Modal } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { List, ListItem, SearchBar } from 'react-native-elements';
+import axios from 'axios';
 
 export default class CalendarView extends React.Component {
-    static navigationOptions = {
-        title: 'Calendar',
-        header: null,
-    };
-    constructor(props) {
-        super(props);
-        this.state = {
-            user_data: this.props.navigation.state.params.user_data,
-            taskData: [],
-            showTaskModal: false,
-            tasksForModal: [],
-            title: '',
-            description: '',
-            deadline: '',
-            showModal: false,
-            currDay: 13,
-            currMonth: 12,
-            currYear: 2017,
-            epoch: 123
-        }
-    };
+        static navigationOptions = {
+            title: 'Calendar',
+            header: null,
+        };
+        constructor(props) {
+            super(props);
+            this.state = {
+                user_data: this.props.navigation.state.params.user_data,
+                all_tasks: [],
+                my_tasks: [],
+                day_tasks: [], //tasks for the day selected on calendar
+                showTaskModal: false,
+                //tasksForModal: [],  tHIS IS NOW DAY_TASKS
+                title: '',
+                description: '',
+                deadline: '',
+                showModal: false,
+                currDay: 13,
+                currMonth: 12,
+                currYear: 2017,
+                currEpoch: 123
+            }
+        };
 
-    componentWillMount() {
-        let hostname = "messageinarawr498.herokuapp.com"; //Alec's IP
-        let taskEndpt = 'https://' + hostname + '/api/tasks';
+        componentWillMount() {
+            let hostname = "messageinarawr498.herokuapp.com";
+            let taskEndpt = 'https://' + hostname + '/api/tasks';
 
-        console.log(this.state.user_data.tasks);
-
-        axios.get(taskEndpt)
-            .then((response) => {
-
-                let newTaskData = [];
-
-                console.log(response.data.data);
-
-                for (let i = 0; i < response.data.data.length; i++) {
-                    let id = i;
-                    let description = response.data.data[i].description;
-                    let title = response.data.data[i].title;
-                    let deadline = response.data.data[i].deadline;
-                    let completed = response.data.data[i].completed;
-
-                    newTaskData.push({
-                        "id": id,
-                        "description": description,
-                        "title": title,
-                        "deadline": deadline,
-                        "completed": completed
-                    })
-                }
-                this.setState({
-                    taskData: newTaskData
+            //Get all tasks and put them in a state
+            //Then set the state of tasks that our current user has
+            axios.get(taskEndpt)
+                .then((response) => {
+                    this.setState({
+                        all_tasks: response.data.data
+                    });
+                    for (i = 0; i < this.state.all_tasks.lenght; i++) {
+                        if (this.state.all_tasks[i].owner === this.state.user_data._id) {
+                            this.state.my_tasks.push(this.state.all_tasks[i]);
+                        }
+                    }
+                })
+                .catch((error) => {
+                    console.log('Error', JSON.stringify(error));
                 });
-                console.log(this.state.taskData.length);
-            })
-            .catch((error) => {
-                console.log('Error', JSON.stringify(error));
-            });
-    }
-
-    getTasksGivenDay = (day) => {
-        console.log("Today is", day.day);
-        tasksOnDay = [];
-
-        let dayTimeStamp = day.timestamp;
-        let tomorrowTimeStamp = day.timestamp + 86400000;
-        let displayTasks = [];
-        let allTasks = this.state.taskData;
-
-        console.log(dayTimeStamp);
-
-        for (let i = 0; i < allTasks.length; i++) {
-            let taskDate = Date.parse(allTasks[i].deadline);
-            console.log(taskDate);
-
-            if (taskDate >= dayTimeStamp && taskDate < tomorrowTimeStamp) {
-                console.log("We got a hit!");
-                displayTasks.push(allTasks[i]);
-            }
-
         }
 
-        console.log(displayTasks.length);
-
-
-        this.setState({
-            showTaskModal: true
-        });
-
-        this.setState({
-            tasksForModal: displayTasks
-        });
-
-        this.setState({
-            day: day.day,
-            month: day.month,
-            year: day.year,
-            epoch: day.timestamp
-        });
-
-        console.log(displayTasks);
-        console.log(this.state.showTaskModal);
-    };
-
-    returnToCalendar = () => {
-        this.setState({
-            showTaskModal: false
-        });
-    };
-
-    updateCompleteTask = (item) => {
-
-        let taskID = item.id;
-        let allTasks = this.state.taskData;
-        task = {
-
-        };
-
-        for (let i = 0; i < allTasks.length; i++) {
-            if (taskID === allTasks[i]._id) {
-                task = allTasks[i];
-                allTasks[i].completed = true;
-                break;
-            }
-        }
-
-        console.log(task);
-
-
-        body = {
-            title: task.title,
-            deadline: task.deadline,
-            owner: task.owner,
-            description: task.description,
-            completed: true
-        };
-
-        let hostname = "messageinarawr498.herokuapp.com"; //Alec's IP
-        let taskEndpt = 'https://' + hostname + '/api/tasks/' + task._id;
-        axios.put(taskEndpt, body)
-            .then((response) => {
-                console.log("put successful.");
-                console.log(response.data);
-                let hostname = "messageinarawr498.herokuapp.com";
-                let bottleEndpt = "https://" + hostname + "/api/users/" + this.state.user_data._id;
-                let completedTasks = this.state.user_data.completedTasks;
-                body = {
-                    completedTasks: completedTasks,
+        getTasksGivenDay = (day) => {
+            console.log("Today is ", day.day);
+            let temp = [];
+            let dayTimeStamp = day.timestamp;
+            let tomorrowTimeStamp = day.timestamp + 86400000;
+            for (let i = 0; i < this.state.all_tasks.length; i++) {
+                let taskDate = Date.parse(this.state.all_tasks[i].deadline);
+                console.log("What we are comparing to timestamps: " + taskDate);
+                if (taskDate >= dayTimeStamp && taskDate < tomorrowTimeStamp) {
+                    console.log("We got a hit!");
+                    temp.push(this.state.all_tasks[i]);
                 }
-                axios.put(bottleEndpt, body)
-                    .then((response) => {
-                        console.log(response.data);
-                    })
-                    .catch((error) => {
-                        console.log('Error With Put', JSON.stringify(error));
-                    })
-            })
-            .catch((err) => {
-                console.log(err);
+            }
+            this.setState({
+                day_tasks: temp
             });
-    };
-
-    submitDataAndReturn = () => {
-        // do stuff and submit some data
-        let hostname = "messageinarawr498.herokuapp.com"; //Alec's IP
-        let taskEndpt = 'https://' + hostname + '/api/tasks';
-
-        let epoch = this.state.epoch;
-
-        console.log(epoch);
-        console.log(this.state.taskData);
-        let title = this.state.title;
-        let owner = this.state.user_data._id;
-
-        body = {
-            title: title,
-            deadline: epoch,
-            owner: owner,
-            description: this.state.description,
-            completed: false
+            this.setState({
+                currDay: day.day,
+                currMonth: day.month,
+                currYear: day.year,
+                currEpoch: day.timestamp
+            });
+            this.setState({
+                showTaskModal: true
+            });
         };
 
-        axios.post(taskEndpt, body)
-            .then((response) => {
-                console.log("post successful.");
-                console.log(response.data);
+        returnToCalendar = () => {
+            this.setState({
+                showTaskModal: false
+            });
+        };
 
-                let newList = this.state.taskData;
-                newList.push(response.data);
-                this.setState({
-                    taskData: newList
+        updateCompleteTask = (item) => {
+            //console.log(item);
+            //update states first
+            for (i = 0; i < this.state.all_tasks.length; i++) {
+                if (this.state.all_tasks[i]._id === item._id) {
+                    this.state.all_tasks[i].completed = true;
+                }
+            }
+            for (i = 0; i < this.state.my_tasks.length; i++) {
+                if (this.state.my_tasks[i]._id === item._id) {
+                    this.state.my_tasks[i].completed = true;
+                }
+            }
+            //now update task info on backend
+            body = {
+                title: item.title,
+                deadline: item.deadline,
+                owner: item.owner,
+                description: item.description,
+                completed: true
+            };
+            let hostname = "messageinarawr498.herokuapp.com"; //Alec's IP
+            let taskEndpt = 'https://' + hostname + '/api/tasks/' + item._id;
+            axios.put(taskEndpt, body)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((err) => {
+                    console.log("Error Updating Task to Complete: " + err);
                 });
+        };
 
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+        addTask = () => {
+            // do stuff and submit some data
+            let hostname = "messageinarawr498.herokuapp.com"; //Alec's IP
+            let taskEndpt = 'https://' + hostname + '/api/tasks';
+            body = {
+                title: this.state.title,
+                deadline: this.state.currEpoch,
+                owner: this.state.user_data._id,
+                description: this.state.description,
+                completed: false
+            };
+            axios.post(taskEndpt, body)
+                .then((response) => {
+                    console.log("Post successful:");
+                    console.log(response.data);
 
 
+                    this.state.all_tasks.push(response.data);
+                    this.state.my_tasks.push(response.data);
+                })
+                .catch((err) => {
+                    console.log("Error adding task:" + err);
+                });
+            this.setState({showModal: false}); // if it doesnt fail or something?, else do error blah blah
+            this.setState({showTaskModal: false});
+        };
 
-
-        this.setState({
-            showModal: false
-        }); // if it doesnt fail or something?, else do error blah blah
-        this.setState({
-            showTaskModal: false
-        });
-    };
-
-    renderSeparator = () => {
-        return ( <
-            View style = {
-                {
-                    height: 1,
-                    backgroundColor: '#CED0CE',
-                    width: '100%',
+        renderSeparator = () => {
+            return ( <
+                View style = {
+                    {
+                        height: 1,
+                        backgroundColor: '#CED0CE',
+                        width: '100%',
+                    }
                 }
-            }
-            />
-        );
-    };
-
-		render() {
+                />
+            );
+        };
+        render() {
 				const { navigate } = this.props.navigation;
 
 				if(this.state.showTaskModal === false) {
@@ -276,7 +182,7 @@ export default class CalendarView extends React.Component {
 											<FlatList
 												style={{backgroundColor: 'white'}}
 												removeClippedSubviews={false}
-												data={this.state.tasksForModal}
+												data={this.state.day_tasks}
 												renderItem={({ item }) => (
 													<ListItem
 														title={item.title}
@@ -288,7 +194,7 @@ export default class CalendarView extends React.Component {
 													}}
 												/>
 		                    )}
-												keyExtractor={item => item.id}
+												keyExtractor={item => item._id}
 												ItemSeparatorComponent={this.renderSeparator}
 											/>
 										</List>
@@ -314,7 +220,7 @@ export default class CalendarView extends React.Component {
 													onChangeText={(text) => this.setState({description: text})}
 													ref={(input) => this.descriptionInput = input}
 												/>
-												<TouchableOpacity style={[styles.modalButton, {marginTop: 20,}]} onPress={this.submitDataAndReturn}>
+												<TouchableOpacity style={[styles.modalButton, {marginTop: 20,}]} onPress={this.addTask}>
 														<Text style={{textAlign: 'center',fontWeight: '700',}}>Done</Text>
 												</TouchableOpacity>
 											</View>
